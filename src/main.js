b5f59ab95a551848af5c849ed7d2a8b585bfe7f8
Timur Sanatov <neizpro@gmail.com>
2025-12-14 23:24:13 +0300

    Изменена проверка на ошибки

--- src/main.js
+++ src/main.js
@@ -62,7 +62,7 @@ function analyzeSalesData(data, options) {
 
   // @TODO: Подготовка итоговой коллекции с нужными полями
 
-  if (!data || data.sellers.length === 0 || !Array.isArray(data.sellers)) {
+  if (!data || data.sellers.length === 0 || !Array.isArray(data.sellers) || !data.purchase_records)  {
     throw new Error("Некорректные входные данные ");
   }
 
@@ -119,7 +119,6 @@ function analyzeSalesData(data, options) {
       .sort((a, b) => b.quantity - a.quantity)
       .slice(0, 10);
   });
-  console.log(sellerStats);
 
   return sellerStats.map((seller) => ({
     seller_id: seller.id,
